1a300a7280b6c0193873613322f7981916bd2992
hiadore <abdullah.husein9192@gmail.com>
2021-06-12 08:37:57 +0700

    feat: add hospitals screen

--- src/components/molecules/HospitalsList/index.js
+++ src/components/molecules/HospitalsList/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {DummyHospital1} from '../../../assets';
+import {colors, fonts} from '../../../utils';
 
 export default function HospitalsList() {
   return (
-    <View>
-      <Text>HospitalsList Component</Text>
+    <View style={styles.container}>
+      <Image source={DummyHospital1} style={styles.picture} />
+      <View>
+        <Text style={styles.title}>Rumah Sakit</Text>
+        <Text style={styles.title}>Citra Bunga Merdeka</Text>
+        <Text style={styles.address}>Jl. Surya Sejahtera 20</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.border.secondary,
+  },
+  picture: {width: 80, height: 60, borderRadius: 11, marginRight: 16},
+  title: {
+    fontSize: 16,
+    fontFamily: fonts.primary.normal,
+    color: colors.text.primary,
+    lineHeight: 19.2,
+  },
+  address: {
+    fontSize: 12,
+    fontFamily: fonts.primary[300],
+    color: colors.text.secondary,
+    marginTop: 6,
+  },
+});
--- src/pages/Hospitals/index.js
+++ src/pages/Hospitals/index.js
@@ -2,21 +2,45 @@ import React from 'react';
 import {ImageBackground, StyleSheet, Text, View} from 'react-native';
 import {ILHospitalBG} from '../../assets';
 import {HospitalsList} from '../../components';
+import {colors, fonts} from '../../utils';
 
 export default function Hospitals() {
   return (
-    <View>
+    <View style={styles.page}>
       <ImageBackground source={ILHospitalBG} style={styles.background}>
-        <Text>Nearby Hospitals</Text>
-        <Text>3 tersedia</Text>
+        <Text style={styles.title}>Nearby Hospitals</Text>
+        <Text style={styles.desc}>3 tersedia</Text>
       </ImageBackground>
-      <HospitalsList />
-      <HospitalsList />
-      <HospitalsList />
+      <View style={styles.content}>
+        <HospitalsList />
+        <HospitalsList />
+        <HospitalsList />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {height: 240},
+  page: {backgroundColor: colors.secondary, flex: 1},
+  background: {height: 240, paddingTop: 30},
+  title: {
+    color: colors.white,
+    textAlign: 'center',
+    fontFamily: fonts.primary[600],
+    fontSize: 20,
+  },
+  desc: {
+    fontSize: 14,
+    fontFamily: fonts.primary[300],
+    color: colors.white,
+    textAlign: 'center',
+    marginTop: 6,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    borderRadius: 20,
+    marginTop: -30,
+    paddingTop: 14,
+  },
 });
